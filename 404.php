222fb9821b6386370855011fa810108e44ac384a
Ruta Mavani <rutamavani01@gmail.com>
2024-11-30 16:59:31 +0530

    game

---
+++ 404.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+<?php // Include Header ?>
+<?php include("includes/header.php") ?>
+<?php // Include Main Header ?>
+<?php include("includes/main-h.php") ?>
+<?php // Body Start ?>
+<body class="bg-dark dark:bg-[#121317]">
+<div class="container-zon flex justify-center items-center h-[80vh]">
+    <?php // 404 Page Error Text Message ?>
+    <h1 class="text-8xl font-bold dark:text-gray-300 text-zinc-800">404</h1>
+</div>
+</body>
+<?php // Body End ?>
---
+++ includes/config.php
@@ -0,0 +1,17 @@
+<?php
+
+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "yes";
+// MySQL Database Name
+$sql_db_name = "zontal";
+
+$con = mysqli_connect($sql_db_host, $sql_db_user, '', $sql_db_name);
+
+// Site URL
+$site_url = "http://192.168.1.7/Zontal-v1.5/"; 
+?>
